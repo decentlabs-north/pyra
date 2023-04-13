db91faa9fabea039d2bb3a1d1117217cefdbd582
Kanan Garaisayev <kenan@luxar.se>
2023-04-13 19:47:37 +0200

    Top five stars with hits

--- designer_html/top-sites.js
+++ designer_html/top-sites.js
@@ -4,15 +4,20 @@ export class TopSites extends Tonic {
   async * render () {
     yield this.html`<p>Loading...</p>`
 
-    // TODO:  const res = await fetch('/silo')
-    // if (res.status !== 200) yield this.html`Failed Loading site`
-    const sites = [{ title: 'fake site', hits: 5 }] // await res.json()
-
+    const res = await fetch('https://pyra.se/silo')
+    if (res.status !== 200) return this.html`Failed Loading site`
+    const sites = await res.json()
     const listItems = []
-    for (const site of sites) {
-      console.log('parsing site info', site) // todo: delete this line
+    sites.sort((a, b) => b.hits - a.hits);
+    for (const [index, site] of sites.entries()) {
+      let stars = '';
+      if (index < 5) {
+        for (let i = 0; i < Math.min(site.hits, 5); i++) {
+          stars += '⭐';
+        }
+      }
       listItems.push(this.html`
-        A website about cars <samp>1000hits</samp>
+        <li><a class="link" href="https://pyra.se/silo/${site.key}" target="_blank">${site.title}</a><samp> hits: ${site.hits+''} </samp> ${stars} </li>
       `)
     }
 
@@ -28,8 +33,8 @@ export class TopSites extends Tonic {
 
   static stylesheet () {
     return `
-      ul {
-
+      .link {
+        color: white
       }
     `
   }
